02e4e3fcbc0b98977920c3eb7a61cb8f4964fb2c
cavanhaman <cavanhaman@hotmail.com>
2018-09-20 20:46:08 -0300

    PUT, DETETE e GET para Cliente

--- src/main/java/com/cavanha/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/cavanha/cursomc/dto/CategoriaDTO.java
@@ -1,10 +1,8 @@
 package com.cavanha.cursomc.dto;
 
 import java.io.Serializable;
-
 import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
-
 import com.cavanha.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
